530b03bc11f664c70c9bc20961a6705710a5981b
Jessica Kerr <jessitron@gmail.com>
2017-04-17 09:28:22 -0500

    name endpoint

--- src/main/java/com/jessitron/SurveyResultResponseController.java
+++ src/main/java/com/jessitron/SurveyResultResponseController.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class SurveyResultResponseController {
 
     @CrossOrigin()
-    @RequestMapping(path = "/surveyResultResponse")
+    @RequestMapping(path = "/vote")
     public SurveyResultResponse surveyResultResponse(
             @RequestBody() SingleResponse response) {
        // TODO: count things
